fe10021afa8ba6b9287f3ea44dcfa377717c72b8
geekbrains <kuznecova_tat88@mail.ru>
2023-10-11 19:46:52 +0300

    first commit

---
+++ src/Lesson_1/Main.java
@@ -0,0 +1,12 @@
+package Lesson_1;
+
+public class Main {
+    /**
+      * Точка входа в программу. С неё всегда всё начинается.
+      *
+      * @param args стандартные аргументы командной строки
+      * */
+    public static void main(String[] args){
+        System.out.println("Hi!");
+    }
+}
